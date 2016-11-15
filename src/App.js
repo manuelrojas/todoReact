256751f5c6d359f8ff204269dc0074a7d4c9eb94
Manuel Rojas <manuel@seesawlabs.com>
2016-11-15 10:40:18 -0600

    Adding new store.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import store from './store';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    let _store = store();
+    console.log('Current state:');
+    console.log(_store.getState());
+    console.log('--------------');
+    console.log('Dispaching ADD_TODO');
+    _store.dispatch({
+      type: 'ADD_TODO',
+      id: 1,
+      text: 'Go shopping'
+    });
+    console.log('Current state:');
+    console.log(_store.getState());
+
+    _store.dispatch({
+      type: 'TOGGLE_TODO',
+      id: 1
+    });
+
+    console.log(_store.getState());
     return (
       <div className="App">
         <div className="App-header">
